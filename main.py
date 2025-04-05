6834cf8f6986c666b40711ff68222633a3e91ba2
Yassine Moutawakkil <yagamiilight1986@gmail.com>
2025-04-05 18:49:34 +0000

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,11 +15,25 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def homepage():
-    return render_template("home.html",
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("home.html",
                             title="ToDo List Accueil",
-                            css_file="home")
+                            css_file="home",
+                            tasks=tasks)
 
 if __name__ == "__main__":
     with app.app_context():
